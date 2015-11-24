48eb0c5e8299d76bcaca18dba6a3c686fa0c1ab3
HarrisonA <radiov@gmail.com>
2015-11-23 17:01:20 -0800

    getElementsByClassName function is complete!

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -1,10 +1,44 @@
-// If life was easy, we could just do things the easy way:
-// var getElementsByClassName = function (className) {
-//   return document.getElementsByClassName(className);
-// };
+var getElementsByClassName = function(className){ 
+	var returnArray = [];  
+  	var actualRecursiveFunction = function (className, startingPoint){  //startingPoint is an array of child nodes
 
-// But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-){
-  // your code here
-};
+  		if (startingPoint === undefined){  //true only for the first getElementsByClassName function call
+  			if($(document.body).hasClass(className)){  //jQuery  check if the body has className
+  				returnArray.push(document.body);  
+  			}
+
+  			var nodes = document.body.childNodes;  // returns an array of all childnodes of <body> 
+  			var length = nodes.length;
+
+  		} else {   //startingPoint was passed in
+
+  			var nodes = startingPoint.childNodes;  //returns an array of childnodes of startingPoint
+  			var length = nodes.length;
+  		}
+
+		for (var i=0; i<length; i++){  		
+  			if ($(nodes[i]).hasClass(className)){   // jQuery check the class of each childNode
+  				returnArray.push(nodes[i]);     	// push the match into the array 
+  			} 
+  		
+  			actualRecursiveFunction(className, nodes[i]);  //recursive call on that child to check its children
+	  		 		
+  		}
+
+		return returnArray;    
+	} // end of actualRecursiveFunction 
+	
+	return actualRecursiveFunction(className);  //returns the complete returnArray after all recursive calls finish
+
+};  //end of getElementsByClassName 
+
+
+
+/*
+
+# Basic Requirements
+
+- [ ] Implement `getElementsByClassName` with your own function in `src/getElementsByClassName.js`, and make the specs pass.
+- [ ] You should use `document.body`, `element.childNodes`, and `element.classList`
+
+*/
